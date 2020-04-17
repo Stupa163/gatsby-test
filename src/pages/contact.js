c5ce4e803f63f0bfa4b7dc87c06ed603d8b16c28
a <a>
2020-04-17 15:28:13 +0200

    formoulaire

---
+++ src/pages/contact.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+const Contact = () => {
+  return (
+    <form name="contact" method="post" data-netlify="true">
+      <p>
+        <label>Your Name: <input type="text" name="name"/></label>
+      </p>
+      <p>
+        <label>Your Email: <input type="email" name="email"/></label>
+      </p>
+      <p>
+        <label>Message: <textarea name="message"></textarea></label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
+  )
+}
+
+export default Contact;

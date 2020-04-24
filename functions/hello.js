ec182f7b561f034f4b93ec090ab8412c7ba32b7c
a <a>
2020-04-24 09:45:46 +0200

    function anonyme 2

--- functions/hello.js
+++ functions/hello.js
@@ -1,6 +1,8 @@
 exports.handler = function(event, context, callback) {
+  console.log('event', event);
+  console.log('context', context);
   callback(null, {
     statusCode: 200,
-    body: "Hello !"
+    body: JSON.stringify("Hello !")
   })
 }

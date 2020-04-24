c06c9cab71df4faf387b2009e3c53973f3d288f1
a <a>
2020-04-24 11:05:47 +0200

    quand on est con

--- functions/getWeather.js
+++ functions/getWeather.js
@@ -2,7 +2,7 @@ const axios = require("axios")
 
 exports.handler = function(event, context, callback) {
   let city = event.queryStringParameters.city
-  axios.get("http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&APPID=16e194441a5b8e37c9f463942da1a344")
+  axios.get("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=16e194441a5b8e37c9f463942da1a344")
     .then((response) => {
       console.log(response)
       let tempKelvin = +response.data.main.temp

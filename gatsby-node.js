f91f7795859dc086d3cb2861e4ebf90fd4378180
a <a>
2020-04-24 17:41:30 +0200

    tout push

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,3 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+exports.createPages = async function({ action, graphql }) {
 
-// You can delete this file if you're not using it
+}

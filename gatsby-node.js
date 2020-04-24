9902ec25299a70064153f453e32372a67503f1ed
a <a>
2020-04-24 21:11:35 +0200

    cBonCaMarchTrolol

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,27 @@
-exports.createPages = async function({ action, graphql }) {
+exports.createPages = async function({ actions, graphql }) {
+
+  const products = await graphql(`
+      query myQuery {
+        allMarkdownRemark {
+          edges {
+            node {
+              frontmatter {
+                path
+              }
+            }
+          }
+        }
+      }
+  `)
+
+  products.data.allMarkdownRemark.edges.forEach(edge => {
+    const path = edge.node.frontmatter.path;
+    actions.createPage({
+        path: path,
+        component: require.resolve('./src/templates/productTemplate.js'),
+        context: {path: path}
+      }
+    )
+  })
 
 }

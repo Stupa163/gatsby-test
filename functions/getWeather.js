2ebb90ffa1e7526ca87e61b5bc2d597f616d5f56
a <a>
2020-04-24 10:52:45 +0200

    wiiiizer

---
+++ functions/getWeather.js
@@ -0,0 +1,22 @@
+const axios = require("axios")
+
+exports.handler = function(event, context, callback) {
+  let city = event.queryStringParameters.city
+  axios.get("http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&APPID=16e194441a5b8e37c9f463942da1a344")
+    .then((response) => {
+      let weather = response.list[0].main
+      let tempKelvin = +weather.temp
+      let tempCelcius = tempKelvin - 273.15
+      callback(null, {
+        statusCode: 400,
+        body: JSON.stringify({ temp: tempCelcius })
+      })
+    })
+    .catch((error) => {
+      console.error(error)
+      callback(null, {
+        statusCode: 400,
+        body: JSON.stringify(error.message)
+      })
+    })
+}

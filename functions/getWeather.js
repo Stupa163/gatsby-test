6b97a232cb0ae2129d2f5a0834c9002e541d635a
a <a>
2020-04-24 11:02:31 +0200

    added wind

--- functions/getWeather.js
+++ functions/getWeather.js
@@ -5,12 +5,12 @@ exports.handler = function(event, context, callback) {
   axios.get("http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&APPID=16e194441a5b8e37c9f463942da1a344")
     .then((response) => {
       console.log(response)
-      let weather = response.data.list[0].main
-      let tempKelvin = +weather.temp
+      let tempKelvin = +response.data.main.temp
       let tempCelcius = tempKelvin - 273.15
+      let wind = response.data.wind.speed
       callback(null, {
         statusCode: 400,
-        body: JSON.stringify({ temp: tempCelcius })
+        body: JSON.stringify({ temp: tempCelcius + 'C°', wind: wind + 'm/s' })
       })
     })
     .catch((error) => {
